64017384f7890346fab714c1796f41aea2b75601
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2022-07-01 20:51:37 +0200

    Drawer Grundgerüst

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,41 @@
 import React from "react"
+import { Drawer, Typography } from "@mui/material"
+import { makeStyles } from "@mui/styles"
+
+const drawerWidth = 240
+
+const useStyles = makeStyles({
+    root: {
+        display: "flex"
+    },
+    drawer: {
+        width: drawerWidth
+    },
+    drawerPaper: {
+        width: drawerWidth
+    }
+})
 
 export default function Layout({ children }) {
+    const classes = useStyles()
+    
     return(
-        <div>
+        <div className={classes.root}>
             {/* app bar */}
 
             {/* side drawer */}
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                anchor="left"
+                classes={{ paper: classes.drawerPaper }}    
+            >
+                <div>
+                    <Typography variant="h5">
+                        Dies ist der Drawer
+                    </Typography>
+                </div>
+            </Drawer>
 
             <div>
                 {children}
