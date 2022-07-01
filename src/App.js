f313813a6b7fa3840063d028efa831abef7bcf17
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2022-07-01 20:21:38 +0200

    Aufsetzen des dark-themes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React from "react";
+import Layout from "./components/Layout";
+import { ThemeProvider } from "@mui/material";
+
 function App() {
+  //mit createTheme() kann das Standard Theme bearbeitet werden.
+  const theme = createTheme({
+    palette: {
+      mode: "dark"
+    },
+  })
+
   return (
-    <div>
-      <h1>This is the App Component</h1>
-    </div>
+    <ThemeProvider theme="theme"> {/** Der Theme Provider gibt das Theme Objekt an alle Kinder weiter */}
+      <CssBaseline /> {/** Entfernt das Margin des Elternelements */}
+      <Layout>
+        <div>
+          <h1>This is the App Component</h1>
+        </div>
+      </Layout>
+    </ThemeProvider>
   );
 }
 
